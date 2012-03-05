09bd9876367a738a3edf4a9094ed92871c3f5eb4
Max Cantor <mxcantor@gmail.com>
2012-03-05 17:34:25 +0200

    first functional shell working

--- trials/GenTrial/Foundation.hs
+++ trials/GenTrial/Foundation.hs
@@ -41,6 +41,8 @@ import qualified Data.Text.Lazy.Encoding
 import Network.Mail.Mime (sendmail)
 #endif
 
+import Data.Text (Text)
+
 -- | The site argument for your application. This can be a good place to
 -- keep settings and values requiring initialization before your application
 -- starts running, such as database connections. Every handler will have
--- trials/GenTrial/Handler/Root.hs
+++ trials/GenTrial/Handler/Root.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Handler.Root where
 
 import Import
@@ -10,8 +11,79 @@ import Import
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
 getRootR :: Handler RepHtml
-getRootR = do
+getRootR = 
     defaultLayout $ do
         h2id <- lift newIdent
         setTitle "GenTrial homepage"
         $(widgetFile "homepage")
+
+
+getAddUserR :: Text -> Text -> Handler ()
+getAddUserR u p = do
+  runDB $ insert $ User u (Just p)
+  setMessage "added user"
+  redirect RootR
+
+
+optionsPersistKey
+  :: (YesodPersist master
+     , PersistEntity a
+     , PersistQuery (YesodPersistBackend master) (GHandler sub master)
+     , PathPiece (Key (YesodPersistBackend master) a)
+     , RenderMessage master msg
+     , PersistEntityBackend a ~ YesodPersistBackend master) 
+  => [Filter a] 
+  -> [SelectOpt a] 
+  -> (Entity a -> msg) 
+  -> GHandler sub master (OptionList (Key (PersistEntityBackend a) a))
+optionsPersistKey filts ords toDisplay = fmap mkOptionList $ do
+    mr <- getMessageRender
+    pairs <- runDB $ selectList filts ords
+    return $ map (\(Entity key value) -> Option
+        { optionDisplay = mr (toDisplay $ Entity key value)
+        , optionInternalValue = key
+        , optionExternalValue = toPathPiece key
+        }) pairs
+
+fooForm :: Maybe Foo -> Form Foo
+fooForm mFoo = 
+  renderDivs $ Foo
+    <$> aopt textField "bar" (fooBar <$> mFoo)
+    <*> areq (selectField (optionsPersistKey [] [Asc UserIdent] (toPathPiece . entityKey))) "user" (fooUser <$> mFoo)
+    <*> areq intField "age" (fooAge <$> mFoo)
+
+getFooR           :: Handler RepHtml
+postFooR          :: Handler RepHtml
+getFooNewR        :: Handler RepHtml
+getFooDetailR     :: FooId -> Handler RepHtml
+putFooDetailR     :: FooId -> Handler RepHtml
+deleteFooDetailR  :: FooId -> Handler RepHtml
+getFooEditR       :: FooId -> Handler RepHtml
+
+
+getFooR = do
+  (allFoos :: [Entity Foo]) <- runDB $ selectList [] []
+  defaultLayout $ $(widgetFile "foo/index")
+
+postFooR = do
+  ((formResult, formWidget), enctype) <- runFormPost $ fooForm Nothing
+  case formResult of
+    FormSuccess foo -> do
+      fooId <- runDB $ insert foo
+      setMessage "successfully added"
+      redirect $ FooDetailR fooId
+    _ -> do 
+      setMessage "form error"
+      redirect FooNewR
+    
+getFooNewR = do
+  ((_, formWidget), enctype) <- generateFormPost $ fooForm Nothing
+  defaultLayout $ $(widgetFile "foo/new")
+    
+  
+getFooDetailR fooId = do
+  foo <- runDB $ get404 fooId
+  defaultLayout $ $(widgetFile "foo/show")
+putFooDetailR fooId = undefined
+deleteFooDetailR fooId = undefined 
+getFooEditR fooId = undefined
