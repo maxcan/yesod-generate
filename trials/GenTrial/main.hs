e317f1da453f5d8cca35c1144932bc959ddc508b
Max Cantor <mxcantor@gmail.com>
2012-03-21 00:15:02 -0700

    got rid of the old scaffold example

--- trials/GenTrial/main.hs
+++
@@ -1,8 +0,0 @@
-import Prelude              (IO)
-import Yesod.Default.Config (fromArgs)
-import Yesod.Default.Main   (defaultMain)
-import Settings             (parseExtra)
-import Application          (getApplication)
-
-main :: IO ()
-main = defaultMain (fromArgs parseExtra) getApplication
